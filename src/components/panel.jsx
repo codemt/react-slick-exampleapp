7811dd9fee4e685966be895e83414a3efe434e55
mithileshtarkar <mithilesh.tarkar@ves.ac.in>
2018-12-05 15:21:51 +0530

    created final design 1 for panel

--- src/components/panel.jsx
+++ src/components/panel.jsx
@@ -14,42 +14,75 @@ class Panel extends Component {
     return (
         <Slider {...settings}>
         <div>
-        <div className="container-fluid">
-        <div className="row">
-            <div className="col testim" id="testim">
-                <div className="wrap"><span id="right-arrow" className="arrow right fa fa-chevron-right"></span><span id="left-arrow" className="arrow left fa fa-chevron-left"></span>
-                    {/*<ul id="testim-dots" className="dots">
-                        <li className="dot active"></li>
-                        <li className="dot"></li>
-                    </ul> */ }
-                    <div id="testim-content" className="cont" >
-                        <div className="active">
-                            <div className="img" ><img src="https://image.ibb.co/hgy1M7/5a6f718346a28820008b4611_750_562.jpg" /></div>
-                            <p style={{paddingTop: 10}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
-                            <h2>Tejas Kor </h2>
-                            <p> UX/UI Designer , Board Infinity </p>
-                            <h2> LOGO </h2>
+
+              <div className="container-fluid">
+                    <div className="row">
+                        <div className="col testim" id="testim">
+                            <div className="wrap"><span id="right-arrow" className="arrow right fa fa-chevron-right"></span><span id="left-arrow" className="arrow left fa fa-chevron-left"></span>
+                                {/*<ul id="testim-dots" className="dots">
+                                    <li className="dot active"></li>
+                                    <li className="dot"></li>
+                                </ul> */ }
+                                <div id="testim-content" className="cont" >
+                                    <div className="active">
+                                        <div className="img" ><img src="https://image.ibb.co/hgy1M7/5a6f718346a28820008b4611_750_562.jpg" /></div>
+                                        <p style={{paddingTop: 10}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+                                        <h2>Tejas Kor </h2>
+                                        <p> UX/UI Designer , Board Infinity </p>
+                                        <h2> LOGO </h2>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        </div>
+                </div>
+
+        </div>
+        <div>
+                    <div className="container-fluid">
+                          <div className="row">
+                              <div className="col testim" id="testim">
+                                  <div className="wrap"><span id="right-arrow" className="arrow right fa fa-chevron-right"></span><span id="left-arrow" className="arrow left fa fa-chevron-left"></span>
+                                      {/*<ul id="testim-dots" className="dots">
+                                          <li className="dot active"></li>
+                                          <li className="dot"></li>
+                                      </ul> */ }
+                                      <div id="testim-content" className="cont" >
+                                          <div className="active">
+                                              <div className="img" ><img src="https://image.ibb.co/hgy1M7/5a6f718346a28820008b4611_750_562.jpg" /></div>
+                                              <p style={{paddingTop: 10}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+                                              <h2>Tejas Kor </h2>
+                                              <p> UX/UI Designer , Board Infinity </p>
+                                              <h2> LOGO </h2>
+                                          </div>
+                                      </div>
+                                  </div>
+                              </div>
+                          </div>
+                </div>
+        </div>
+        <div>
+               <div className="container-fluid">
+                    <div className="row">
+                        <div className="col testim" id="testim">
+                            <div className="wrap"><span id="right-arrow" className="arrow right fa fa-chevron-right"></span><span id="left-arrow" className="arrow left fa fa-chevron-left"></span>
+                                {/*<ul id="testim-dots" className="dots">
+                                    <li className="dot active"></li>
+                                    <li className="dot"></li>
+                                </ul> */ }
+                                <div id="testim-content" className="cont" >
+                                    <div className="active">
+                                        <div className="img" ><img src="https://image.ibb.co/hgy1M7/5a6f718346a28820008b4611_750_562.jpg" /></div>
+                                        <p style={{paddingTop: 10}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+                                        <h2>Tejas Kor </h2>
+                                        <p> UX/UI Designer , Board Infinity </p>
+                                        <h2> LOGO </h2>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </div>
-        </div>
-    </div>
-        </div>
-        <div>
-          <h3>2</h3>
-        </div>
-        <div>
-          <h3>3</h3>
-        </div>
-        <div>
-          <h3>4</h3>
-        </div>
-        <div>
-          <h3>5</h3>
-        </div>
-        <div>
-          <h3>6</h3>
+              </div>
         </div>
       </Slider>
     )
