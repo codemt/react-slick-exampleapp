eeee8a5c862a52f9866c4bbe399de35ad1b92229
mithileshtarkar <mithilesh.tarkar@ves.ac.in>
2018-12-05 12:56:59 +0530

    slider panel added for creating slider - made changes from js to jsx

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import Panel from './components/panel';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+       <Panel />
+      </div>
+    );
+  }
+}
+
+export default App;
