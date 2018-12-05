7b4118485e7692838ce593d9057fe99b25f3a5ae
mithileshtarkar <mithilesh.tarkar@ves.ac.in>
2018-12-05 15:16:22 +0530

    designed custom ui for slider content 1

--- src/components/panel.jsx
+++ src/components/panel.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import Slider from "react-slick";
-
+import '../assets/css/slider.css';
+import '../assets/css/testimonials.css';
 class Panel extends Component {
   render() {
     var settings = {
@@ -13,7 +14,25 @@ class Panel extends Component {
     return (
         <Slider {...settings}>
         <div>
-          <h3>1</h3>
+        <div className="container-fluid">
+        <div className="row">
+            <div className="col testim" id="testim">
+                <div className="wrap"><span id="right-arrow" className="arrow right fa fa-chevron-right"></span><span id="left-arrow" className="arrow left fa fa-chevron-left"></span>
+                    {/*<ul id="testim-dots" className="dots">
+                        <li className="dot active"></li>
+                        <li className="dot"></li>
+                    </ul> */ }
+                    <div id="testim-content" className="cont" >
+                        <div className="active">
+                            <div className="img" ><img src="https://image.ibb.co/hgy1M7/5a6f718346a28820008b4611_750_562.jpg" /></div>
+                            <p style={{paddingTop: 10}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+                            <h2>Tejas Kor </h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
         </div>
         <div>
           <h3>2</h3>
