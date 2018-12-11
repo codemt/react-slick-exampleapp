ef6eb631e334b0d96eca0156b83064357ab868ca
mithileshtarkar <mithilesh.tarkar@ves.ac.in>
2018-12-11 10:17:40 +0530

    created job card and searchbox design

--- src/components/job2/searchbox.jsx
+++ src/components/job2/searchbox.jsx
@@ -8,32 +8,35 @@ import DateSelect from '../jobs/selectboxs/date';
  class Job2 extends Component {
   render() {
     return (
-        <section class="paddingTop-60 paddingBottom-100">
-                <div class="">
+        <section className="paddingTop-60 paddingBottom-100" style={{marginBottom:'-150px'}}>
+
+
+                <div className="">             
+              </div> {/*    END row    */}
                 { /* ROW -1 
-                        <div class="row">
-                        <div class="col-12 z-index-10" data-offset-top-md="-120">
-                        <div class="input-group bg-white rounded p-5 padding-y-md-50 shadow-v2" style={{marginBottom:'-40px'}}>
-                        <div class=" col-2 input-group prepend">
+                        <div className="row">
+                        <div className="col-12 z-index-10" data-offset-top-md="-120">
+                        <div className="input-group bg-white rounded p-5 padding-y-md-50 shadow-v2" style={{marginBottom:'-40px'}}>
+                        <div className=" col-2 input-group prepend">
                            
                             
-                            <i class="fas fa-align-justify" style={{marginTop:'10px'}}></i>
+                            <i className="fas fa-align-justify" style={{marginTop:'10px'}}></i>
                             
                            
                             
                         </div>
-                        <div class="col-10 input-group prepend">
+                        <div className="col-10 input-group prepend">
 
-                                <select class="form-control rounded-left-0 rounded-right-0">
+                                <select className="form-control rounded-left-0 rounded-right-0">
                                 <option selected disabled default> Search Job </option>
                                 <option value="1">Best Selling</option>
                                 <option value="2">Top rated</option>
                                 <option value="3">Newest</option>
                                 <option value="4">Top authors</option>
                             </select>
-                             <input type="text" class="form-control" placeholder="By keywords,designation,skills .." />
-                            <div class="input-group-append">
-                                <button class="btn btn-primary">Search</button>
+                             <input type="text" className="form-control" placeholder="By keywords,designation,skills .." />
+                            <div className="input-group-append">
+                                <button className="btn btn-primary">Search</button>
                             </div>
                                 
                     
@@ -47,17 +50,19 @@ import DateSelect from '../jobs/selectboxs/date';
                     */}
                   
             { /* Row 2 */ }
-                    <div class="row">
-                    <div class="col-12 z-index-10" data-offset-top-md="-120">
-                    <div class="input-group bg-white rounded p-5 padding-y-md-50 shadow-v2">
-                    <div class=" col-4 input-group prepend">
+                    <div className="row">
+                      {/*END col-lg-3 */}
+               
+                    <div className="col-12 z-index-10" data-offset-top-md="-120">
+                    <div className="input-group bg-white rounded p-5 padding-y-md-50 shadow-v2">
+                    <div className=" col-4 input-group prepend">
                        
-                    <i class="fas fa-align-justify" style={{marginTop:'10px',marginRight:'10px'}}></i>
-                    <input type="text" class="form-control" placeholder="By keywords,designation,skills .." />
+                    <i className="fas fa-align-justify" style={{marginTop:'10px',marginRight:'10px'}}></i>
+                    <input type="text" className="form-control" placeholder="By keywords,designation,skills .." />
                        
                         
                     </div>
-                    <div class="col-8 input-group prepend">
+                    <div className="col-8 input-group prepend">
 
                        {/* <SelectBox /> */ }
                          <SelectExperience /> 
@@ -66,8 +71,8 @@ import DateSelect from '../jobs/selectboxs/date';
                          <DateSelect />
               
                 
-                        <div class="input-group-append" style={{marginLeft:'10px'}}>
-                            <button class="btn btn-primary">Search</button>
+                        <div className="input-group-append" style={{marginLeft:'10px'}}>
+                            <button className="btn btn-primary">Search</button>
                         </div>
                             
                 
@@ -79,7 +84,7 @@ import DateSelect from '../jobs/selectboxs/date';
                 </div> 
                     
 
-                    </div>
+                
       </section>
     )
   }
